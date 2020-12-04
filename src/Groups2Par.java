abaf44094c5773e793b2f9a82845238c961c8662
mihnea.pretorian <13440122+pmihnea@users.noreply.github.com>
2020-12-04 10:26:27 +0100

    added two parallel versions

--- src/Groups2Par.java
+++ src/Groups2Par.java
@@ -45,40 +45,44 @@ public class Groups2Par {
     }
 
     public int countNodeGroups_v1(Stream<Node> relatedStream) {
-        final HashMap<Integer, GroupHolder> item2Group = relatedStream.collect(
-                getSupplier_v1(),
-                getAccumulator_v1(),
-                getCombiner_v1()
-        );
-        long nGroups = item2Group.values().stream().mapToInt(gw -> System.identityHashCode(gw.group)).distinct().count();
+        final HashMap<Integer, HashSet<Integer>> item2Group = relatedStream
+                .map(this::node2Set)
+                .collect(
+                        getSupplier_v1(),
+                        getAccumulator_v1(),
+                        getCombiner_v1()
+                );
+        long nGroups = item2Group.values().stream().mapToInt(gw -> System.identityHashCode(gw)).distinct().count();
         return (int) nGroups;
     }
 
-    public int countNodeGroups(Stream<Node> relatedStream) {
+    public int countNodeGroups_v2(Stream<Node> relatedStream) {
         final LinkedList<HashSet<Integer>> groups = relatedStream
-                .map(node -> {
-                    final HashSet<Integer> group = new HashSet<>();
-                    for (int relation = 0; relation < node.nodeRelations.length(); relation++) {
-                        if (node.nodeRelations.charAt(relation) == '1') {
-                            group.add(relation);
-                        }
-                    }
-                    return group;
-                }).reduce(
-                        getIdentity(),
-                        getAccumulator(),
-                        getCombiner()
+                .map(this::node2Set).reduce(
+                        getIdentity_v2(),
+                        getAccumulator_v2(),
+                        getCombiner_v2()
                 );
         return groups.size();
     }
 
-    private LinkedList<HashSet<Integer>> getIdentity() {
+    private HashSet<Integer> node2Set(Node node) {
+        final HashSet<Integer> group = new HashSet<>();
+        for (int relation = 0; relation < node.nodeRelations.length(); relation++) {
+            if (node.nodeRelations.charAt(relation) == '1') {
+                group.add(relation);
+            }
+        }
+        return group;
+    }
+
+    private LinkedList<HashSet<Integer>> getIdentity_v2() {
         return new LinkedList<>();
     }
 
-    private BinaryOperator<LinkedList<HashSet<Integer>>> getCombiner() {
+    private BinaryOperator<LinkedList<HashSet<Integer>>> getCombiner_v2() {
         return (groups1, groups2) -> {
-            final BiFunction<LinkedList<HashSet<Integer>>, HashSet<Integer>, LinkedList<HashSet<Integer>>> accumulator = getAccumulator();
+            final BiFunction<LinkedList<HashSet<Integer>>, HashSet<Integer>, LinkedList<HashSet<Integer>>> accumulator = getAccumulator_v2();
             LinkedList<HashSet<Integer>> result = new LinkedList<>(groups1);
             for (HashSet<Integer> group2 : groups2) {
                 result = accumulator.apply(result, group2);
@@ -87,7 +91,7 @@ public class Groups2Par {
         };
     }
 
-    private BiFunction<LinkedList<HashSet<Integer>>, HashSet<Integer>, LinkedList<HashSet<Integer>>> getAccumulator() {
+    private BiFunction<LinkedList<HashSet<Integer>>, HashSet<Integer>, LinkedList<HashSet<Integer>>> getAccumulator_v2() {
         return (groups, group) -> {
             final LinkedList<HashSet<Integer>> result = new LinkedList<>();
             HashSet<Integer> mergedGroup = null;
@@ -111,44 +115,22 @@ public class Groups2Par {
         };
     }
 
-    private Supplier<HashMap<Integer, GroupHolder>> getSupplier_v1() {
+    private Supplier<HashMap<Integer, HashSet<Integer>>> getSupplier_v1() {
         return () -> new HashMap<>();
     }
 
-    private BiConsumer<HashMap<Integer, GroupHolder>, Node> getAccumulator_v1() {
-        return (item2group, node) -> {
-            int root = node.nodeId;
-            String rootRelations = node.nodeRelations;
-
-            //initialize root group if it doesn't exist yet
-            GroupHolder rootGroup = item2group.get(root);
-            if (rootGroup == null) {
-                rootGroup = new GroupHolder();
-                rootGroup.group.add(root);
-                item2group.put(root, rootGroup);
-            }
-
-            //root relations need to have the same group as root
-            for (int relation = 0; relation < rootRelations.length(); relation++) {
-                if (rootRelations.charAt(relation) == '1') {
-                    GroupHolder relationGroup = item2group.get(relation);
-                    if (relationGroup == null) {
-                        rootGroup.group.add(relation);
-                        relationGroup = new GroupHolder(rootGroup.group);
-                        item2group.put(relation, relationGroup);
-                    } else if (relationGroup.group != rootGroup.group /*object identity*/) {
-                        // merge the groups into one
-                        rootGroup.group.addAll(relationGroup.group);
-                        // all added elements need to have the same group
-                        for (Integer item : relationGroup.group) {
-                            GroupHolder itemGroup = item2group.get(item);
-                            if (itemGroup == null) {
-                                itemGroup = new GroupHolder(rootGroup.group);
-                                item2group.put(item, itemGroup);
-                            } else {
-                                itemGroup.group = rootGroup.group;
-                            }
-                        }
+    private BiConsumer<HashMap<Integer, HashSet<Integer>>, HashSet<Integer>> getAccumulator_v1() {
+        return (item2group, group) -> {
+            HashSet<Integer> mergedGroup = new HashSet<>(group);// could we reuse an existing set?
+            for (Integer relation : group) {
+                HashSet<Integer> relationGroup = item2group.get(relation);
+                item2group.put(relation, mergedGroup);
+                if (relationGroup != null && relationGroup != mergedGroup /*object identity*/) {
+                    // merge the groups into one
+                    mergedGroup.addAll(relationGroup);
+                    // all added elements need to have the same group
+                    for (Integer item : relationGroup) {
+                        item2group.put(item, mergedGroup);
                     }
                 }
             }
@@ -156,38 +138,21 @@ public class Groups2Par {
     }
 
 
-    private BiConsumer<HashMap<Integer, GroupHolder>, HashMap<Integer, GroupHolder>> getCombiner_v1() {
+    private BiConsumer<HashMap<Integer, HashSet<Integer>>, HashMap<Integer, HashSet<Integer>>> getCombiner_v1() {
         return (item2group1, item2group2) -> {
-            for (Integer nodeId : item2group2.keySet()) {
-                final GroupHolder relationsGroup1 = item2group1.get(nodeId);
-                final GroupHolder relationsGroup2 = item2group2.get(nodeId);
-                if (relationsGroup1 == null) {
-                    // the node does not exist in the map-1 so it can be copied over from map-2 to map-1
-                    item2group1.put(nodeId, relationsGroup2);
-                    // the node relations will be copied at their turn
-                } else if (relationsGroup1.group != relationsGroup2.group /*object identity*/) {
-                    // the node exists in both maps with different groups
-                    // merge the two groups into one
-                    // all the new elements need to have the same group
-                    for (Integer item : relationsGroup2.group) {
-                        if (relationsGroup1.group.add(item)) {
-                            GroupHolder itemGroup = item2group1.get(item);
-                            if (itemGroup == null) {
-                                itemGroup = new GroupHolder(relationsGroup1.group);
-                            } else {
-                                relationsGroup1.group.addAll(itemGroup.group);
-                                itemGroup.group = relationsGroup1.group;
-                            }
-                            item2group1.put(item, itemGroup);
-                        }
-                    }
+            final BiConsumer<HashMap<Integer, HashSet<Integer>>, HashSet<Integer>> accumulator = getAccumulator_v1();
+            final HashSet<Integer> groupsMerged = new HashSet<>(item2group2.size());
+            item2group2.values().forEach(group -> {
+                if (!groupsMerged.contains(System.identityHashCode(group))) {
+                    accumulator.accept(item2group1, group);
+                    groupsMerged.add(System.identityHashCode(group));
                 }
-            }
+            });
         };
     }
 
     public static void main(String[] args) {
-        //runTests(false);
+        runTests(false);
         runTests(true);
     }
 
@@ -226,7 +191,7 @@ public class Groups2Par {
                 "1"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test1", 1, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test1", 1, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     private static void test2(boolean parallel) {
@@ -235,7 +200,7 @@ public class Groups2Par {
                 "01"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test2", 2, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test2", 2, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     private static void test3(boolean parallel) {
@@ -245,7 +210,7 @@ public class Groups2Par {
                 "001"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test3", 3, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test3", 3, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     private static void test4(boolean parallel) {
@@ -255,7 +220,7 @@ public class Groups2Par {
                 "001"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test4", 2, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test4", 2, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     private static void test5(boolean parallel) {
@@ -265,7 +230,7 @@ public class Groups2Par {
                 "001"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test5", 1, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test5", 1, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     private static void test6(boolean parallel) {
@@ -276,7 +241,7 @@ public class Groups2Par {
                 "0011"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test6", 1, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test6", 1, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     private static void test7(boolean parallel) {
@@ -288,7 +253,7 @@ public class Groups2Par {
                 "10011"
         );
         Groups2Par fn = new Groups2Par();
-        assertTest("Test7", 1, () -> fn.countNodeGroups(toNodeStream(related, parallel)), parallel);
+        assertTest("Test7", 1, () -> fn.countNodeGroups_v1(toNodeStream(related, parallel)), parallel);
     }
 
     /*
@@ -306,13 +271,13 @@ public class Groups2Par {
                 char[] value = new char[N];
                 row++;
                 for (int j = 0; j < N; j++) {
-                    value[j] = (row + j) % 2 == 0 ? '1' : '0';
+                    value[j] = (row - j) % 10 == 0 ? '1' : '0';
                 }
                 return new Node(row, new String(value));
             }
         }).limit(N);
         Groups2Par fn = new Groups2Par();
-        assertTest("Test8-" + N, 2,
-                () -> fn.countNodeGroups(parallel ? related.parallel() : related), parallel);
+        assertTest("Test8-" + N, 10,
+                () -> fn.countNodeGroups_v1(parallel ? related.parallel() : related), parallel);
     }
 }
